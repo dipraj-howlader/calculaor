de57d0814d203dd45a4cb4502080ff295b67c180
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-01-29 13:39:41 +0600

    creating cal

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandButton = document.querySelector('[data-previous-operand]')
+const currentOperandButton = document.querySelector('[data-current-operand]')
